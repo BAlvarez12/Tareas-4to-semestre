6e54bd1e23d07f0b472230b72214cbf800a40d4a
BAlvarez12 <160418757+BAlvarez12@users.noreply.github.com>
2024-07-30 23:53:36 -0600

    Calculadora

---
+++ src/calculadora/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author Bomiki
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Cal calculadora = new Cal();
+        calculadora.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
